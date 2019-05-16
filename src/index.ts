8855c869ebdfd73605af43f725e1d1c2b4f7a467
rayliao <littlefish@littlefishdeiMac.local>
2019-05-16 10:56:08 +0800

    refactor

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
-import * as styles from './index.module.css'
+// import styles from './index.module.css'
 
 // tslint:disable-next-line:no-console
 console.log(
-  `<div class="${styles.root}" style="color: ${styles.primary}"></div>`
+  `<div></div>`
 )
